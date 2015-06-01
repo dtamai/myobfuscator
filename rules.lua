667e783ddf26c752c043139409a6252d6d780c17
Daniel Tamai <daniel.tamai@gmail.com>
2015-05-31 21:50:05 -0300

    Improve rules sample with random generators

--- rules.lua
+++ rules.lua
@@ -1,10 +1,16 @@
+require "utils"
+
+math.randomseed(os.time())
+
 users = {}
 
 function users.__row__(fieldsIdx, row)
-  row[fieldsIdx["email"]] = "alice.bob@example.com"
+  name = firstName() .. " " .. lastName()
+  row[fieldsIdx["name"]] = name
+  row[fieldsIdx["email"]] = string.lower(string.gsub(name, " ", ".")) .. "_" .. string.random(6, "%l%d") .. "@example.com"
   return row
 end
 
-function users.name(orignal_value)
-  return "Alice Bob"
+function users.name(originalValue)
+  return originalValue .. " " .. lastName()
 end
---
+++ utils.lua
@@ -0,0 +1,104 @@
+-- Random Strings
+-- http://lua-users.org/wiki/RandomStrings
+local Chars = {}
+for Loop = 0, 255 do
+  Chars[Loop+1] = string.char(Loop)
+end
+local String = table.concat(Chars)
+
+local Built = {['.'] = Chars}
+
+local AddLookup = function(CharSet)
+  local Substitute = string.gsub(String, '[^'..CharSet..']', '')
+  local Lookup = {}
+  for Loop = 1, string.len(Substitute) do
+    Lookup[Loop] = string.sub(Substitute, Loop, Loop)
+  end
+  Built[CharSet] = Lookup
+
+  return Lookup
+end
+
+function string.random(Length, CharSet)
+  -- Length (number)
+  -- CharSet (string, optional); e.g. %l%d for lower case letters and digits
+
+  local CharSet = CharSet or '.'
+
+  if CharSet == '' then
+    return ''
+  else
+    local Result = {}
+    local Lookup = Built[CharSet] or AddLookup(CharSet)
+    local Range = table.getn(Lookup)
+
+    for Loop = 1,Length do
+      Result[Loop] = Lookup[math.random(1, Range)]
+    end
+
+    return table.concat(Result)
+  end
+end
+-- END Random Strings
+
+function firstName()
+  local names = {
+    "Alice",
+    "Bob",
+    "Clara",
+    "Diego",
+    "Elise",
+    "Fontaine",
+    "Gahiji",
+    "Hathor",
+    "Isamar",
+    "Jake",
+    "Kalere",
+    "Lio",
+    "Miyuki",
+    "Nereu",
+    "Olivia",
+    "Pillan",
+    "Qiana",
+    "Rudra",
+    "Skee",
+    "Tiara",
+    "Ugo",
+    "Vasilisa",
+    "Wilson",
+    "Xin",
+    "Young",
+    "Zara"
+  }
+  return names[math.random(1,#names)]
+end
+
+function lastName()
+  local names = {
+    "Andersson",
+    "Baek",
+    "Cardozo",
+    "Dion",
+    "Evans",
+    "Flores",
+    "Huang",
+    "Ivanov",
+    "Jara",
+    "King",
+    "Lopez",
+    "Matei",
+    "Nakamura",
+    "Ortiz",
+    "Parker",
+    "Quinn",
+    "Rosario",
+    "Singh",
+    "Tomic",
+    "Ueda",
+    "Van Dyk",
+    "Xu",
+    "Yoon",
+    "Zupan"
+  }
+  return names[math.random(1,#names)]
+end
